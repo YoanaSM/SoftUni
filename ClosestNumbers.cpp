6a5c9c0616a623729a92b63297b4fb3b58996c49
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2023-03-25 21:19:43 +0200

    Add files via upload

---
+++ ClosestNumbers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<climits>
+#include<cassert>
+
+
+const int MAX_SIZE = 150;
+int main()
+{
+	int N1;
+	std::cin >> N1;
+	assert(N1 > 0);
+	int arr1[MAX_SIZE];
+	int subtraction = 0;
+	int  minSubtract = INT_MAX;
+	for (size_t i = 0; i < N1; i++)
+	{
+		std::cin >> arr1[i];
+
+	}
+	for (size_t i = 0; i < N1; i++)
+	{
+
+		for (size_t j = i + 1; j < N1; j++)
+		{
+			subtraction = abs(arr1[i] - arr1[j]);
+			if (subtraction < minSubtract)
+			{
+				minSubtract = subtraction;
+			}
+		}
+
+	}
+
+	std::cout << minSubtract << std::endl;
+	return 0;
+}
+
