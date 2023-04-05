bc1cd054a0f6104d6fe78e484c84b444e4a72e8d
YoanaM <ioko256@abv.bg>
2023-04-05 20:57:26 +0300

    MostFrequentNumber

---
+++ TitleCase.cpp
@@ -0,0 +1,44 @@
+//
+// Created by YoanaM on 4/5/2023.
+//
+#include<iostream>
+#include<cstring>
+#include<sstream>
+
+std :: string capitalize(const std :: string & input)//reference bc we won't uncopy the string we receive and we wont change the string
+{
+
+   std ::  ostringstream oStr;
+
+
+    bool capitalize = true;
+    for (size_t idx = 0; idx < input.length(); idx++)
+    {
+        char c = input[idx];
+        if (c >='a' && c <='z'|| c >= 'A' && c<='Z')// if it is a letter
+        {
+            if(capitalize)
+            {
+                if (c >= 'a' && c <='z')
+
+                    c -= 32;
+                    capitalize = false; // turn the small letter into capital
+
+            }
+        }
+        else
+            capitalize = true;
+        oStr << c;
+
+    }
+
+
+    return oStr.str();
+}
+int main()
+{
+    std :: string input;
+    getline(std::cin,input);
+    std::cout << capitalize(input )<< std::endl;
+    return 0;
+}
